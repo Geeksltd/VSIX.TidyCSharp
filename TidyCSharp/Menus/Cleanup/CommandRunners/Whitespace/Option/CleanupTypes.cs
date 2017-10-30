4205c594e22109d8585ab4f338cfea697c11ee5c
alireza tabatabaee <sa.tabatabaee@gmail.com>
2017-10-31 03:27:13 +0330

    Splitting Cleanup items => WhitespaceNormalizeer

--- TidyCSharp/Menus/Cleanup/CommandRunners/Whitespace/Option/CleanupTypes.cs
+++ TidyCSharp/Menus/Cleanup/CommandRunners/Whitespace/Option/CleanupTypes.cs
@@ -6,25 +6,40 @@ namespace Geeks.VSIX.TidyCSharp.Cleanup.NormalizeWhitespace
     [Flags]
     public enum CleanupTypes
     {
-        [CleanupItem()]
-        Remove_DBL_Between_Methods_Statements = 0x01,
-        [CleanupItem(FirstOrder = 1, Title = "Remove DBL Between Methods Statements")]
-        Remove_DBL_Between_Class_Members = 0x02,
-        [CleanupItem()]
+        [CleanupItem(FirstOrder = 1, Title = "Trim the file")]
+        Trim_The_File = 0x80,
+
+        [CleanupItem(FirstOrder = 1, Title = "Remove duplicate blank lines between namespace members")]
         Remove_DBL_Between_Namespace_Members = 0x04,
-        [CleanupItem()]
+
+        [CleanupItem(FirstOrder = 1, Title = "Remove duplicate blank lines between class members")]
+        Remove_DBL_Between_Class_Members = 0x02,
+
+        [CleanupItem(FirstOrder = 1, Title = "Remove duplicate blank lines between method statement")]
+        Remove_DBL_Between_Methods_Statements = 0x01,
+
+        [CleanupItem(FirstOrder = 1, Title = "Remove duplicate blank lines between comments")]
         Remove_DBL_Inside_Comments = 0x08,
-        [CleanupItem()]
+
+        [CleanupItem(FirstOrder = 1, Title = "Remove duplicate blank lines between \"Usings ..\"")]
         Remove_DBL_Inside_Usings = 0x10,
-        [CleanupItem()]
+
+        [CleanupItem(FirstOrder = 1, Title = "Remove blank lines before and after \"{\" and \"}\" of blocks")]
         Remove_BLs_after_Open_Bracket_and_Before_Close_Brackets = 0x20,
+
         /// <summary>
         /// Insert_one_space_between_the_comment_delimiter_and_the_comment_text,
         /// </summary>
+        [CleanupItem(FirstOrder = 1, Title = "Insert space before comment text : \\\\TODO: => \\\\ TODO:")]
         Insert_Space_Before_Comment_Text = 0x40,
-        Trim_The_File = 0x80,
+
+        [CleanupItem(FirstOrder = 1, Title = "")]
         Adding_Blank_after_Method_Close_Bracket = 0x100,
+
+        [CleanupItem(FirstOrder = 1, Title = "")]
         Adding_Blank_after_Block_Close_Bracket = 0x200,
+
+        [CleanupItem(FirstOrder = 1, Title = "")]
         Remove_Brackets_of_block_that_has_only_one_statement_with_length_shorter_than_70_chars = 0x400,
     }
 
