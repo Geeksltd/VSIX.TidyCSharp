c17ef0a8d9f0e891ac9c03aab2aee0ede95ad26b
alireza tabatabaee <sa.tabatabaee@gmail.com>
2017-11-08 00:13:06 +0330

    correct menu - use base libraries

--- TidyCSharp/Infra/Utility/Utils.cs
+++ TidyCSharp/Infra/Utility/Utils.cs
@@ -70,52 +70,5 @@ namespace GeeksAddin
                 ErrorNotification.EmailError(err);
             }
         }
-
-        //public static IEnumerable<string> SplitCommandLine(string commandLine)
-        //{
-        //    var inQuotes = false;
-
-        //    return commandLine.Split(c =>
-        //    {
-        //        if (c == '\"')
-        //            inQuotes = !inQuotes;
-        //        return !inQuotes && c == ' ';
-        //    }).Select(arg => arg.Trim().TrimMatchingQuotes())
-        //      .Where(arg => !string.IsNullOrEmpty(arg));
-        //}
-
-        //public static IEnumerable<string> Split(this string str, Func<char, bool> controller)
-        //{
-        //    var nextPiece = 0;
-
-        //    for (var c = 0; c < str.Length; c++)
-        //    {
-        //        if (controller(str[c]))
-        //        {
-        //            yield return str.Substring(nextPiece, c - nextPiece);
-        //            nextPiece = c + 1;
-        //        }
-        //    }
-
-        //    yield return str.Substring(nextPiece);
-        //}
-
-        //public static string TrimMatchingQuotes(this string input, char quote = '\"')
-        //{
-        //    if ((input.Length >= 2) &&
-        //        (input[0] == quote) && (input[input.Length - 1] == quote))
-        //        return input.Substring(1, input.Length - 2);
-
-        //    return input;
-        //}
-
-        //public static bool ContainsAny(this string str, params string[] subStrings)
-        //{
-        //    foreach (var subString in subStrings)
-        //        if (str.Contains(subString))
-        //            return true;
-
-        //    return false;
-        //}
     }
 }
