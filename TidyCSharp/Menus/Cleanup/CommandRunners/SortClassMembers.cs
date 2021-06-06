44bf193467720893ed7cfbe610d14ec249f31de4
miladkakouli <milad.kakoli@yahoo.com>
2021-06-06 13:07:21 +0100

    using directive read onlymode completed

--- TidyCSharp/Menus/Cleanup/CommandRunners/SortClassMembers.cs
+++ TidyCSharp/Menus/Cleanup/CommandRunners/SortClassMembers.cs
@@ -6,84 +6,84 @@ using System.Linq;
 
 namespace Geeks.VSIX.TidyCSharp.Cleanup
 {
-    public class SortClassMembers : CodeCleanerCommandRunnerBase, ICodeCleaner
-    {
-        public override SyntaxNode CleanUp(SyntaxNode initialSourceNode)
-        {
-            var modifiedSourceNode = SortClassMembersHelper(initialSourceNode);
-            if (IsReportOnlyMode &&
-                   !IsEquivalentToUnModified(modifiedSourceNode))
-            {
-                //this.CollectMessages(syntaxRewriter.GetReport());
-                return initialSourceNode;
-            }
-            return modifiedSourceNode;
-        }
+	public class SortClassMembers : CodeCleanerCommandRunnerBase, ICodeCleaner
+	{
+		public override SyntaxNode CleanUp(SyntaxNode initialSourceNode)
+		{
+			var modifiedSourceNode = SortClassMembersHelper(initialSourceNode);
+			//if (IsReportOnlyMode &&
+			//	   !IsEquivalentToUnModified(modifiedSourceNode))
+			//{
+			//	//this.CollectMessages(syntaxRewriter.GetReport());
+			//	return initialSourceNode;
+			//}
+			return modifiedSourceNode;
+		}
 
-        public static SyntaxNode SortClassMembersHelper(SyntaxNode initialSource)
-        {
-            var classes =
-                initialSource
-                .DescendantNodes()
-                .Where(x => x is ClassDeclarationSyntax)
-                .OfType<ClassDeclarationSyntax>();
+		public static SyntaxNode SortClassMembersHelper(SyntaxNode initialSource)
+		{
+			var classes =
+				initialSource
+				.DescendantNodes()
+				.Where(x => x is ClassDeclarationSyntax)
+				.OfType<ClassDeclarationSyntax>();
 
-            var newClassesDic = new Dictionary<ClassDeclarationSyntax, ClassDeclarationSyntax>();
+			var newClassesDic = new Dictionary<ClassDeclarationSyntax, ClassDeclarationSyntax>();
 
-            foreach (var classNode in classes)
-            {
-                var newClassNode = SortClassMemebersHelper(classNode);
-                newClassesDic.Add(classNode, newClassNode);
-            }
+			foreach (var classNode in classes)
+			{
+				var newClassNode = SortClassMemebersHelper(classNode);
+				newClassesDic.Add(classNode, newClassNode);
+			}
 
-            initialSource =
-                initialSource
-                    .ReplaceNodes
-                    (
-                        classes,
-                        (oldNode1, oldNode2) =>
-                        {
-                            var newClass = newClassesDic[oldNode1];
-                            if (oldNode1 != newClass) return newClass;
-                            return oldNode1;
-                        }
-                    );
+			initialSource =
+				initialSource
+					.ReplaceNodes
+					(
+						classes,
+						(oldNode1, oldNode2) =>
+						{
+							var newClass = newClassesDic[oldNode1];
+							if (oldNode1 != newClass) return newClass;
+							return oldNode1;
+						}
+					);
 
-            return initialSource;
-        }
+			return initialSource;
+		}
 
-        public static ClassDeclarationSyntax SortClassMemebersHelper(ClassDeclarationSyntax classNode)
-        {
-            var methods = classNode.Members.Where(x => x is MethodDeclarationSyntax).ToList();
-            var firstMethod = methods.FirstOrDefault();
-            if (firstMethod == null) return classNode;
+		public static ClassDeclarationSyntax SortClassMemebersHelper(ClassDeclarationSyntax classNode)
+		{
+			var methods = classNode.Members.Where(x => x is MethodDeclarationSyntax).ToList();
+			var firstMethod = methods.FirstOrDefault();
+			if (firstMethod == null) return classNode;
 
-            var methodAnnotation = new SyntaxAnnotation();
-            var annotatedClassNode = classNode.ReplaceNode(firstMethod, firstMethod.WithAdditionalAnnotations(methodAnnotation));
+			var methodAnnotation = new SyntaxAnnotation();
+			var annotatedClassNode = classNode.ReplaceNode(firstMethod, firstMethod.WithAdditionalAnnotations(methodAnnotation));
 
-            var constructors = annotatedClassNode.Members.Where(x => x is ConstructorDeclarationSyntax).ToList();
-            if (constructors.Any() == false) return classNode;
+			var constructors = annotatedClassNode.Members.Where(x => x is ConstructorDeclarationSyntax).ToList();
+			if (constructors.Any() == false) return classNode;
 
-            var constructorsToMoveList = new List<SyntaxNode>();
+			var constructorsToMoveList = new List<SyntaxNode>();
 
-            foreach (var constructorItem in constructors)
-            {
-                if (firstMethod.SpanStart < constructorItem.SpanStart)
-                {
-                    constructorsToMoveList.Add(constructorItem);
-                }
-            }
+			foreach (var constructorItem in constructors)
+			{
+				if (firstMethod.SpanStart < constructorItem.SpanStart)
+				{
+					constructorsToMoveList.Add(constructorItem);
+				}
+			}
 
-            if (constructorsToMoveList.Any())
-            {
-                annotatedClassNode = annotatedClassNode.RemoveNodes(constructorsToMoveList, SyntaxRemoveOptions.KeepNoTrivia);
-                var annotatedMethod = annotatedClassNode.GetAnnotatedNodes(methodAnnotation).FirstOrDefault();
-                annotatedClassNode = annotatedClassNode.InsertNodesBefore(annotatedMethod, constructorsToMoveList);
+			if (constructorsToMoveList.Any())
+			{
+				annotatedClassNode = annotatedClassNode.RemoveNodes(constructorsToMoveList, SyntaxRemoveOptions.KeepNoTrivia);
+				var annotatedMethod = annotatedClassNode.GetAnnotatedNodes(methodAnnotation).FirstOrDefault();
+				annotatedClassNode = annotatedClassNode.InsertNodesBefore(annotatedMethod, constructorsToMoveList);
 
-                return annotatedClassNode;
-            }
+				return annotatedClassNode;
+			}
 
-            return classNode;
-        }
-    }
+			return classNode;
+		}
+	}
 }
--- TidyCSharp/Menus/Cleanup/CommandRunners/UsingDirectiveOrganizer.cs
+++ TidyCSharp/Menus/Cleanup/CommandRunners/UsingDirectiveOrganizer.cs
@@ -6,51 +6,56 @@ using Geeks.GeeksProductivityTools.Utils;
 using Geeks.VSIX.TidyCSharp.Cleanup.Infra;
 using Geeks.VSIX.TidyCSharp.Menus.Cleanup.SyntaxNodeExtractors;
 using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
 using System;
 
 namespace Geeks.VSIX.TidyCSharp.Cleanup
 {
-    public class UsingDirectiveOrganizer : CodeCleanerCommandRunnerBase, ICodeCleaner
-    {
-        public override SyntaxNode CleanUp(SyntaxNode initialSourceNode)
-        {
+	public class UsingDirectiveOrganizer : CodeCleanerCommandRunnerBase, ICodeCleaner
+	{
+		public override SyntaxNode CleanUp(SyntaxNode initialSourceNode)
+		{
 
-            try
-            {
-                Microsoft.VisualStudio.Shell.ThreadHelper.ThrowIfNotOnUIThread();
+			try
+			{
+				Microsoft.VisualStudio.Shell.ThreadHelper.ThrowIfNotOnUIThread();
 
-                var window = ProjectItemDetails.ProjectItem.Open(Constants.vsViewKindCode);
-                //window = ProjectItemDetails.ProjectItem.Document.DTE
-                //    .ItemOperations.OpenFile(ProjectItemDetails.ProjectItem.Document.Path
-                //    , Constants.vsViewKindCode);
-                //window.Activate();
-                ProjectItemDetails.ProjectItem.Document.Activate();
-                ProjectItemDetails.ProjectItem.Document.DTE.ExecuteCommand(UsingsCommands.REMOVE_AND_SORT_COMMAND_NAME);
-                var modified = ProjectItemDetails.ProjectItem.ToSyntaxNode();
-                var unm = this.UnModifiedProjectItemDetails;
-                if (IsReportOnlyMode &&
-                    !IsEquivalentToUnModified(modified))
-                {
-                    this.CollectMessages(new ChangesReport(initialSourceNode)
-                    {
-                        LineNumber = 1,
-                        Column = 1,
-                        Message = "Your Using usage is not good",
-                        Generator = nameof(UsingDirectiveOrganizer)
-                    });
-                    return initialSourceNode;
-                }
-                ProjectItemDetails.ProjectItem.Document.Save();
-                //window.Document.Save();
-            }
-            catch (Exception e)
-            {
-                ErrorNotification.WriteErrorToFile(e);
-                ErrorNotification.EmailError(e);
-                ProcessActions.GeeksProductivityToolsProcess();
-            }
+				var window = ProjectItemDetails.ProjectItem.Open(Constants.vsViewKindCode);
+				//window = ProjectItemDetails.ProjectItem.Document.DTE
+				//    .ItemOperations.OpenFile(ProjectItemDetails.ProjectItem.Document.Path
+				//    , Constants.vsViewKindCode);
+				//window.Activate();
+				ProjectItemDetails.ProjectItem.Document.Activate();
+				ProjectItemDetails.ProjectItem.Document.DTE.ExecuteCommand(UsingsCommands.REMOVE_AND_SORT_COMMAND_NAME);
+				EnvDTE.TextDocument doc = (EnvDTE.TextDocument)(ProjectItemDetails.ProjectItem.Document.Object("TextDocument"));
+				var p = doc.StartPoint.CreateEditPoint();
+				string s = p.GetText(doc.EndPoint);
+				var modified = SyntaxFactory.ParseSyntaxTree(s);
 
-            return ProjectItemDetails.ProjectItem.ToSyntaxNode();
-        }
-    }
+				if (IsReportOnlyMode &&
+					!IsEquivalentToUnModified(modified.GetRoot()))
+				{
+					this.CollectMessages(new ChangesReport(initialSourceNode)
+					{
+						LineNumber = 1,
+						Column = 1,
+						Message = "Your Using usage is not good",
+						Generator = nameof(UsingDirectiveOrganizer)
+					});
+					ProjectItemDetails.ProjectItem.Document.Undo();
+					return initialSourceNode;
+				}
+				ProjectItemDetails.ProjectItem.Document.Save();
+				//window.Document.Save();
+			}
+			catch (Exception e)
+			{
+				ErrorNotification.WriteErrorToFile(e);
+				ErrorNotification.EmailError(e);
+				ProcessActions.GeeksProductivityToolsProcess();
+			}
+
+			return ProjectItemDetails.ProjectItem.ToSyntaxNode();
+		}
+	}
 }
